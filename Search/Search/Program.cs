6228fd150c7d0a6ba4a99018d390dfb66981e805
Andrey Saitgalin <andrey.saitgalin@gmail.com>
2013-02-13 01:27:15 +0400

    Added library for mp3 tags (it requires some changes but works)

--- Search/Search/Program.cs
+++ Search/Search/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Security.Cryptography;
+using System.Data.SqlClient;
 
 namespace Search
 {
@@ -11,7 +12,23 @@ namespace Search
     {
         static void Main(string[] args)
         {
-            SQLFileWorker worker = new SQLFileWorker();
+            //SqlConnection connection;
+            //SqlCommand command;
+            //SqlDataReader dataReader;
+            //string connectionString = "Server = 93.175.2.42\\SQLEXPRESS; Database = MIPTSearch; User Id=DuXeN0N; Password=2191019;";
+            //connection = new SqlConnection(connectionString);
+            //connection.Open();
+            //command = new SqlCommand("SELECT * FROM Extensions", connection);
+            //dataReader = command.ExecuteReader();
+            //dataReader.Read();
+            //Console.WriteLine(dataReader[1]);
+            //dataReader.Close();
+            TagsReader tr = new TagsReader();
+            tr.LoadTags(@"d:\Music\100_rock_best\012. AC-DC - T.N.T..mp3");
+            Console.WriteLine(tr.Title);
+            Console.WriteLine(tr.Artist);
+            Console.WriteLine(tr.Album);
+            Console.WriteLine(tr.Year);
         }
     }
 }
